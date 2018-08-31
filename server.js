10ad539f8e64d21c44ee6876a2818bdafe6152af
Steve Christensen <steve.christensen@protonmail.com>
2018-08-31 14:48:46 -0500

    fix: update server.js to use sendFile method instead of sendfile (deprecated) for serving files. chore: add .bak files to .gitignore

--- server.js
+++ server.js
@@ -8,21 +8,21 @@ const sendOptions = {
 /* serves main page */
 app.get("/", function(req, res) {
   console.log('Serving root.');
-  res.sendfile('index.html');
+  res.sendFile( __dirname + '/index.html');
 });
 
 /* serves all the static files assuming that static files have a
  * period followed by an extension */
 app.get(/^(.+\..+)$/, function(req, res){
    console.log('static file request : ' + req.params);
-   res.sendfile( __dirname + req.params[0]);
+   res.sendFile( __dirname + req.params[0]);
 });
 
 /* Deep links will not have an extension. Respond with index.html
  * and let the web app handle the routing */
 app.get(/(.+)$/, (req, res) => {
   console.log('serving deep link : ' + req.params);
-  res.sendfile('index.html');
+  res.sendFile( __dirname + '/index.html');
 });
 
  var port = process.env.PORT || 8002;
